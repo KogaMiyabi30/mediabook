bd61674ea7a10ced3e324ee72fdc5f136bacf4c1
Khari <Kingryuki20@gmail.com>
2017-05-16 14:17:06 -0400

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-5.times do |x|
-    User.create(username: "awesomeStudent#{x}", email: "student#{x}@gmail.com", password: "password", password_confirmation: "password")
-end
-p "Test users created"
+User.create!([
+  {email: "kharibaker@hotmail.com", password: "password", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 2, current_sign_in_at: "2017-05-08 19:57:24", last_sign_in_at: "2017-05-08 19:04:33", current_sign_in_ip: "::1", last_sign_in_ip: "::1", username: "kharibaker", name: "Khari", age: 23, bio: "Im a ninja", school: nil, work: nil},
+  {email: "student0@gmail.com", password: "password", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 0, current_sign_in_at: nil, last_sign_in_at: nil, current_sign_in_ip: nil, last_sign_in_ip: nil, username: "awesomeStudent0", name: nil, age: nil, bio: nil, school: nil, work: nil},
+  {email: "student1@gmail.com", password: "password", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 0, current_sign_in_at: nil, last_sign_in_at: nil, current_sign_in_ip: nil, last_sign_in_ip: nil, username: "awesomeStudent1", name: nil, age: nil, bio: nil, school: nil, work: nil},
+  {email: "student2@gmail.com", password: "password", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 0, current_sign_in_at: nil, last_sign_in_at: nil, current_sign_in_ip: nil, last_sign_in_ip: nil, username: "awesomeStudent2", name: nil, age: nil, bio: nil, school: nil, work: nil},
+  {email: "student3@gmail.com", password: "password", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 0, current_sign_in_at: nil, last_sign_in_at: nil, current_sign_in_ip: nil, last_sign_in_ip: nil, username: "awesomeStudent3", name: nil, age: nil, bio: nil, school: nil, work: nil},
+  {email: "student4@gmail.com", password: "password", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 0, current_sign_in_at: nil, last_sign_in_at: nil, current_sign_in_ip: nil, last_sign_in_ip: nil, username: "awesomeStudent4", name: nil, age: nil, bio: nil, school: nil, work: nil}
+])
+Friendship.create!([
+  {user_id: nil, friend_id: nil, state: "pending", friended_at: nil},
+  {user_id: 1, friend_id: 6, state: "pending", friended_at: nil}
+])
